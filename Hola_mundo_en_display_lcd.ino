9bf83167897736ddfb2bb20c03af6350e643ae5f
joselopezx <jose0xlopez@gmail.com>
2025-11-21 19:48:09 -0600

    Add files via upload

---
+++ Hola_mundo_en_display_lcd.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("Hola mundo");
+  delay(1000);
+}
